696feb0fd94da30d0ded8251724473c0f3f116b7
Migz De Chavez <dmgzky@gmail.com>
2019-02-24 15:44:29 +0800

    add Home route

--- src/app/App.js
+++ src/app/App.js
@@ -3,14 +3,34 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
 import { NavBar, Main } from "./common";
+import { Home } from "./home";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          {/* 
+            ...
+            HEADER/NAV LAYOUT
+            _main header/navigation for this application
+            _holds base links and global search controls
+            ...
+          */}
           <NavBar />
-          <Main />
+
+          {/* 
+            ...
+            MAIN CONTAINER
+            _holds content for each views/pages
+            _with react-router holder
+            ...
+          */}
+          <Main>
+            <Switch>
+              <Route path="/" exact component={Home} />
+            </Switch>
+          </Main>
         </div>
       </Router>
     );
