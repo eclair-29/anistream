c8c209b637266ed80c497df3e6eeae03da1e9141
Migz De Chavez <dmgzky@gmail.com>
2019-02-25 10:30:07 +0800

    add home reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import commonReducer from "./app/common/duck";
+import homeReducer from "./app/home/duck";
 
 const rootReducer = combineReducers({
-  common: commonReducer
+  common: commonReducer,
+  home: homeReducer
 });
 
 export default rootReducer;
