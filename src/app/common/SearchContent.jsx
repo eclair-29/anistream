1c4f14f9af3f1fe0a742a5d05c1b9a095f9ffaa6
Migz De Chavez <dmgzky@gmail.com>
2019-02-25 10:35:51 +0800

    modified some base styles at utils

--- src/app/common/SearchContent.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class SearchContent extends Component {
-  render() {
-    return <div className="search-content" />;
-  }
-}
-
-export default SearchContent;
