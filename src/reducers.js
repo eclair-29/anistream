aeb391d4ccaf36a24847052936416783989a3ce5
Migz De Chavez <dmgzky@gmail.com>
2019-02-26 11:15:05 +0800

    add reducers for anime list and categories

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
-import commonReducer from "./app/common/duck";
+import commonReducer from "./app/layout/duck";
 import homeReducer from "./app/home/duck";
+import animeListReducer from "./app/animelist/duck";
+import categoriesReducer from "./app/categories/duck";
 
 const rootReducer = combineReducers({
   common: commonReducer,
-  home: homeReducer
+  home: homeReducer,
+  animeList: animeListReducer,
+  categories: categoriesReducer
 });
 
 export default rootReducer;
